628a8fbeba716a1bfe9719386efe6915c11d6527
sbribrowne <sbribrowne@gmail.com>
2017-10-28 09:24:37 -0500

    add ajax element

--- gif.js
+++ gif.js
@@ -1,5 +1,7 @@
 
-      var animals = ["Moose", "Cat", "Dog", "Horse", "Girrafe"];
+      var animals = ["Moose", "Cat", "Dog", "Horse", "Elephant"];
+
+//ADDING BUTTONS
 
       // display data
       function renderButtons() {
@@ -26,3 +28,41 @@
       });
 
       renderButtons(); //display initial movies array
+
+
+//MAKING GIFS APPEAR ON CLICK WITH AJAX
+
+$("button").on("click", function() {
+      $("#gifPlacement").empty();
+      var title = $(this).attr("data-name");
+
+      // AJAX Stuff
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        title + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        }) .done(function(response) {
+         
+          var results = response.data;
+          console.log(queryURL);
+
+          for (var i = 0; i < results.length; i++) {
+              var gifHolder = $("<div class='section'>");
+
+              var rating = results[i].rating;
+              var p = $("<p>").text("Rating: " + rating);
+
+              var showGif = $("<img>");
+              showGif.attr("src", results[i].images.fixed_height.url);
+
+              gifHolder.append(showGif);
+              gifHolder.append(p);
+
+              $("#gifPlacement").append(gifHolder);
+            }
+        });
+    });
+
+//MAKING GIFS STILL AND ANIMATED ON CLICK
